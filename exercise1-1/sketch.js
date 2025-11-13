8c5defd7c0a2def0ca6623ef807c6ed85745acc2
ljv531-glitch <ljv531@york.ac.uk>
2025-11-13 12:00:46 +0000

    Update sketch.js

--- exercise1-1/sketch.js
+++ exercise1-1/sketch.js
@@ -1,15 +1,34 @@
+let starPositions = [];
+
 function setup() {
-    createCanvas(400, 300);
-    noStroke();
+  createCanvas(400, 300);
+  noStroke();
+  background(0, 0, 50);
+
+ 
+  for (let i = 0; i < 3; i++) {
+    let x = random(50, width - 50); 
+    let y = random(50, height - 50);
+    starPositions.push({x: x, y: y});
+  }
+
+ 
+  for (let pos of starPositions) {
+    star(pos.x, pos.y);
+  }
 }
 
 function draw() {
-    background(0, 0, 50);
-    fill(255, 234, 0);
-    triangle(width / 2, height / 2 - 50, width / 2 - 20, height / 2, width / 2 + 20, height / 2);
-    triangle(width / 2 - 50, height / 2 - 20, width / 2, height / 2 - 20, width / 2, height / 2 + 10);
-    triangle(width / 2 + 50, height / 2 - 20, width / 2, height / 2 - 20, width / 2, height / 2 + 10);
-    triangle(width / 2 - 20, height / 2 - 5, width / 2, height / 2 + 10, width / 2 - 35, height / 2 + 30);
-    triangle(width / 2, height / 2 + 10, width / 2 + 20, height / 2 - 5, width / 2 + 35, height / 2 + 30);
+  
+}
 
+
+function star(x, y) {
+  fill(255, 234, 0);
+
+  triangle(x, y - 50, x - 20, y, x + 20, y);          
+  triangle(x - 50, y - 20, x, y - 20, x, y + 10);     
+  triangle(x + 50, y - 20, x, y - 20, x, y + 10);     
+  triangle(x - 20, y - 5, x, y + 10, x - 35, y + 30); 
+  triangle(x, y + 10, x + 20, y - 5, x + 35, y + 30); 
 }
